a44ce59dbf73234e4c725b38eaa00070339892cd
Uladzislau, Danik (contracted) <Danik.Uladzislau@contracted.pmi.com>
2025-03-24 11:46:26 +0100

    Optimum - 3 examples handling

--- src/main/java/optym/Optimum.java
+++ src/main/java/optym/Optimum.java
@@ -4,46 +4,96 @@ import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
 
 public class Optimum {
 
-    public static double epsilonZero = 0;
-    public static double[] epsilons = { 1, 0.1, 0.001, 0.00001, 0.000000001, 0.0000000000001, 0.000000000000001 };
-    public static int maxIter = 100000;
-    public static int[] iterations = { 3, 4, 5, 7, 10, 15, 20, 30, 40, 50, 70, 100 };
+    public static final Boolean example1 = false; // (100 - x)^2
+    public static final Boolean example2 = false; // 1/3x^3 + 1/2x^2 - 5x + 2
+    public static final Boolean example3 = true; // 1/3x^3 + 2.05x^2 - 9x + 15
 
-    public static double a = -10, b = -3;
+    public static final double epsilonZero = 0;
+    public static final double[] epsilons = { 0.1, 0.01, 0.001, 0.00001, 0.000000001, 0.0000000000001 };
+    public static final int maxIter = 100000;
+    public static final int[] iterations = { 3, 5, 7, 10, 15, 30, 50 };
+
+    public static final double X = example1 ? 100.0 : (example2 ? (-1.0 + Math.sqrt(21)) / 2.0 : (-41.0 - Math.sqrt(5281)) / 20.0);
+    public static final Boolean FIND_MIN = example1 || example2;
+    public static final Boolean INCLUDE_DETAILS = false;
+
+    public static double a = example1 ? 60 : (example2 ? 1 : -10);
+    public static double b = example1 ? 150 : (example2 ? 2 : -3);
+
     public static double f(double x) {
-        return (1.0/3.0) * Math.pow(x, 3) + 2.05 * Math.pow(x, 2) - 9 * x + 15;
+        if (example1) {
+            return Math.pow(100 - x, 2);
+        }
+        if (example2) {
+            return (1.0/3.0) * Math.pow(x, 3) + (1.0/2.0) * Math.pow(x, 2) - 5 * x + 2;
+        }
+        if (example3) {
+            return (1.0/3.0) * Math.pow(x, 3) + 2.05 * Math.pow(x, 2) - 9 * x + 15;
+        }
+        return 0;
     }
 
     public static double[] fp(double x) {
-        DerivativeStructure xDS = new DerivativeStructure(1, 3, 0, x);
-        DerivativeStructure term1 = xDS.pow(3).multiply(1.0/3.0);
-        DerivativeStructure term2 = xDS.pow(2).multiply(2.05);
-        DerivativeStructure term3 = xDS.multiply(-9);
-        DerivativeStructure result = term1.add(term2).add(term3).add(15);
-        double value = result.getValue();
-        double derivative1 = result.getPartialDerivative(1);
-        double derivative2 = result.getPartialDerivative(2);
-        double derivative3 = result.getPartialDerivative(3);
-        return new double[]{value, derivative1, derivative2, derivative3};
+        if (example1) {
+            DerivativeStructure xDS = new DerivativeStructure(1, 2, 0, x);
+            DerivativeStructure result = xDS.multiply(-1).add(100).pow(2);
+
+            double value = result.getValue();
+            double derivative1 = result.getPartialDerivative(1);
+            double derivative2 = result.getPartialDerivative(2);
+            return new double[]{value, derivative1, derivative2};
+        }
+        if (example2) {
+            DerivativeStructure xDS = new DerivativeStructure(1, 3, 0, x);
+            DerivativeStructure term1 = xDS.pow(3).multiply(1.0/3.0);
+            DerivativeStructure term2 = xDS.pow(2).multiply(1.0/2.0);
+            DerivativeStructure term3 = xDS.multiply(-5);
+            DerivativeStructure result = term1.add(term2).add(term3).add(2);
+
+            double value = result.getValue();
+            double derivative1 = result.getPartialDerivative(1);
+            double derivative2 = result.getPartialDerivative(2);
+            double derivative3 = result.getPartialDerivative(3);
+            return new double[]{value, derivative1, derivative2, derivative3};
+        }
+        if (example3) {
+            DerivativeStructure xDS = new DerivativeStructure(1, 3, 0, x);
+            DerivativeStructure term1 = xDS.pow(3).multiply(1.0/3.0);
+            DerivativeStructure term2 = xDS.pow(2).multiply(2.05);
+            DerivativeStructure term3 = xDS.multiply(-9);
+            DerivativeStructure result = term1.add(term2).add(term3).add(15);
+
+            double value = result.getValue();
+            double derivative1 = result.getPartialDerivative(1);
+            double derivative2 = result.getPartialDerivative(2);
+            double derivative3 = result.getPartialDerivative(3);
+            return new double[]{value, derivative1, derivative2, derivative3};
+        }
+        return null;
     }
 
     public static class OptimizationResult {
         public double optimum;
         public double optimumValue;
         public int iterations;
-        public double a, b;
+        public String details;
 
-        public OptimizationResult(double optimum, double optimumValue, int iterations, double a, double b) {
+        public OptimizationResult(double optimum, double optimumValue, int iterations) {
             this.optimum = optimum;
             this.optimumValue = optimumValue;
             this.iterations = iterations;
-            this.a = a;
-            this.b = b;
+        }
+
+        public OptimizationResult(double optimum, double optimumValue, int iterations, String details) {
+            this.optimum = optimum;
+            this.optimumValue = optimumValue;
+            this.iterations = iterations;
+            this.details = details;
         }
 
         @Override
         public String toString() {
-            return "x: " + optimum + ", f(x): " + optimumValue + ", iterations: " + iterations;
+            return "x: " + optimum + ", f(x): " + optimumValue + ", iterations: " + iterations + (INCLUDE_DETAILS ? ("\n" + details) : "");
         }
     }
 
@@ -68,10 +118,13 @@ public class Optimum {
         return fibs;
     }
 
-    public static OptimizationResult dwudzielnaSearch(double a, double b, double epsilon, int iterationLimit, boolean findMin) {
+    public static OptimizationResult dwudzielnaSearch(double a, double b, double epsilon, int iterationLimit) {
         double x_s = (a + b) / 2.0;
         double L = b - a;
         int iteration = 0;
+        StringBuilder details = new StringBuilder();
+        details.append("iter: x, f(x), a, b, L\n");
+        details.append(iteration + ": " + x_s + ", " + f(x_s) + ", " + a + ", " + b + ", " + L + "\n");
 
         while (L > epsilon && iteration < iterationLimit) {
             iteration++;
@@ -82,7 +135,7 @@ public class Optimum {
             double f_x1 = f(x1);
             double f_x2 = f(x2);
 
-            if (findMin) {
+            if (FIND_MIN) {
                 if (f_x1 < f_xs) {
                     b = x_s;
                     x_s = x1;
@@ -105,11 +158,12 @@ public class Optimum {
                     b = x2;
                 }
             }
+            details.append(iteration + ": " + x_s + ", " + f(x_s) + ", " + a + ", " + b + ", " + L + "\n");
         }
-        return new OptimizationResult(x_s, f(x_s), iteration, a, b);
+        return new OptimizationResult(x_s, f(x_s), iteration, details.toString());
     }
 
-    public static OptimizationResult fibonacciSearch(double a, double b, double epsilon, int iterationLimit, boolean findMin) {
+    public static OptimizationResult fibonacciSearch(double a, double b, double epsilon, int iterationLimit) {
         double L = b - a;
         int n;
         if (epsilon > 0) {
@@ -127,12 +181,15 @@ public class Optimum {
         double x2 = a + ((double)fibSeq[n - 1] / fibSeq[n]) * (b - a);
 
         int iter = 0;
+        StringBuilder details = new StringBuilder();
+        details.append("iter: x1, f(x1), x2, f(x2)\n");
+        details.append(iter + ": " + x1 + ", " + f(x1) + ", " + x2 + ", " + f(x2) + "\n");
         while (n > 1) {
             iter++;
             if (epsilon > 0 && Math.abs(x2 - x1) < epsilon) {
                 break;
             }
-            if (findMin) {
+            if (FIND_MIN) {
                 if (f(x1) < f(x2)) {
                     b = x2;
                     x2 = x1;
@@ -157,20 +214,23 @@ public class Optimum {
                     x2 = a + ((double)fibSeq[n - 1] / fibSeq[n]) * (b - a);
                 }
             }
+            details.append(iter + ": " + x1 + ", " + f(x1) + ", " + x2 + ", " + f(x2) + "\n");
             if (epsilon == 0 && iter >= iterationLimit) {
                 break;
             }
         }
         double x = (a + b) / 2.0;
-        return new OptimizationResult(x, f(x), iter, a, b);
+        return new OptimizationResult(x, f(x), iter, details.toString());
     }
 
     public static OptimizationResult bisectionMethod(double a, double b, double epsilon, int iterationLimit) {
         if (fp(a)[1] * fp(b)[1] >= 0) {
-            return new OptimizationResult(Double.NaN, Double.NaN, 0, a, b);
+            return new OptimizationResult(Double.NaN, Double.NaN, 0);
         }
         int iter = 0;
         double x_mid;
+        StringBuilder details = new StringBuilder();
+        details.append("iter: x, f(x), a, b\n");
         while (true) {
             iter++;
             x_mid = (a + b) / 2.0;
@@ -189,13 +249,20 @@ public class Optimum {
             } else {
                 a = x_mid;
             }
+            details.append(iter + ": " + x_mid + ", " + f(x_mid) + ", " + a + ", " + b + "\n");
         }
-        return new OptimizationResult(x_mid, f(x_mid), iter, a, b);
+        return new OptimizationResult(x_mid, f(x_mid), iter, details.toString());
     }
 
     public static OptimizationResult newtonMethod(double a, double b, double epsilon, int iterationLimit) {
+        if (fp(a)[2] * fp(b)[2] < 0 || fp(a)[3] * fp(b)[3] < 0) {
+            return new OptimizationResult(Double.NaN, Double.NaN, 0);
+        }
         double x0 = (fp(a)[3] * fp(a)[1] > 0) ? a : b;
         int iter = 0;
+        StringBuilder details = new StringBuilder();
+        details.append("iter: x0, f(x0)\n");
+        details.append(iter + ": " + x0 + ", " + f(x0) + "\n");
         while (true) {
             iter++;
             double x1 = x0 - fp(x0)[1] / fp(x0)[2];
@@ -211,13 +278,17 @@ public class Optimum {
                 }
             }
             x0 = x1;
+            details.append(iter + ": " + x0 + ", " + f(x0) + "\n");
         }
-        return new OptimizationResult(x0, f(x0), iter, a, b);
+        return new OptimizationResult(x0, f(x0), iter, details.toString());
     }
 
     public static OptimizationResult secantMethod(double a, double b, double epsilon, int iterationLimit) {
         double x0 = fp(a)[1] > 0 ? b : a;
         int iter = 0;
+        StringBuilder details = new StringBuilder();
+        details.append("iter: x0, f(x0)\n");
+        details.append(iter + ": " + x0 + ", " + f(x0) + "\n");
         while (true) {
             iter++;
             double x1;
@@ -238,16 +309,21 @@ public class Optimum {
                 }
             }
             x0 = x1;
+            details.append(iter + ": " + x0 + ", " + f(x0) + "\n");
         }
-        return new OptimizationResult(x0, f(x0), iter, a, b);
+        return new OptimizationResult(x0, f(x0), iter, details.toString());
     }
 
     public static void main(String[] args) {
-        runDwudzielna();
-        runFibonacciego();
-        runBisekcji();
-        runStycznych();
-        runSiecznych();
+        if (example1 || example3) {
+            runDwudzielna();
+            runFibonacciego();
+        }
+        if (example2 || example3) {
+            runBisekcji();
+            runStycznych();
+            runSiecznych();
+        }
     }
 
     private static void runDwudzielna() {
@@ -255,24 +331,14 @@ public class Optimum {
         for (double e : epsilons) {
             System.out.println("\n=== Epsilon: " + e + " ===");
 
-            System.out.print("Maximum: ");
-            OptimizationResult resultMax = dwudzielnaSearch(a, b, e, maxIter, false);
-            System.out.println(resultMax);
-
-            System.out.print("Minimum: ");
-            OptimizationResult resultMin = dwudzielnaSearch(a, b, e, maxIter, true);
-            System.out.println(resultMin);
+            OptimizationResult result = dwudzielnaSearch(a, b, e, maxIter);
+            System.out.println(result);
         }
         for (int i : iterations) {
             System.out.println("\n=== Max iterations: " + i + " ===");
 
-            System.out.print("Maximum: ");
-            OptimizationResult resultMax = dwudzielnaSearch(a, b, epsilonZero, i, false);
-            System.out.println(resultMax);
-
-            System.out.print("Minimum: ");
-            OptimizationResult resultMin = dwudzielnaSearch(a, b, epsilonZero, i, true);
-            System.out.println(resultMin);
+            OptimizationResult result = dwudzielnaSearch(a, b, epsilonZero, i);
+            System.out.println(result);
         }
     }
 
@@ -281,24 +347,14 @@ public class Optimum {
         for (double e : epsilons) {
             System.out.println("\n=== Epsilon: " + e + " ===");
 
-            System.out.print("Maximum: ");
-            OptimizationResult resultMax = fibonacciSearch(a, b, e, maxIter, false);
-            System.out.println(resultMax);
-
-            System.out.print("Minimum: ");
-            OptimizationResult resultMin = fibonacciSearch(a, b, e, maxIter, true);
-            System.out.println(resultMin);
+            OptimizationResult result = fibonacciSearch(a, b, e, maxIter);
+            System.out.println(result);
         }
         for (int i : iterations) {
             System.out.println("\n=== Max iterations: " + i + " ===");
 
-            System.out.print("Maximum: ");
-            OptimizationResult resultMax = fibonacciSearch(a, b, epsilonZero, i, false);
-            System.out.println(resultMax);
-
-            System.out.print("Minimum: ");
-            OptimizationResult resultMin = fibonacciSearch(a, b, epsilonZero, i, true);
-            System.out.println(resultMin);
+            OptimizationResult result = fibonacciSearch(a, b, epsilonZero, i);
+            System.out.println(result);
         }
     }
 
